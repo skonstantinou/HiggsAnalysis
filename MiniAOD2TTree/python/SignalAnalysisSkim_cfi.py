bc5bbdc663caf0e018ca6f7b00189496732c13b5
S. Lehti <sami.lehti@cern.ch>
2018-06-13 12:41:48 +0200

    SignalAnalysisSkim: updated signal triggers

--- MiniAOD2TTree/python/SignalAnalysisSkim_cfi.py
+++ MiniAOD2TTree/python/SignalAnalysisSkim_cfi.py
@@ -3,13 +3,13 @@ import FWCore.ParameterSet.Config as cms
 skim = cms.EDFilter("SignalAnalysisSkim",
     TriggerResults = cms.InputTag("TriggerResults::HLT"),
     HLTPaths       = cms.vstring(
-	"HLT_LooseIsoPFTau50_Trk30_eta2p1_MET80_v",
-        "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET90_v",
-        "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET110_v",
-        "HLT_LooseIsoPFTau50_Trk30_eta2p1_MET120_v",
-        "HLT_VLooseIsoPFTau120_Trk50_eta2p1_v",
-        "HLT_VLooseIsoPFTau140_Trk50_eta2p1_v",
-        "HLT_LooseIsoPFTau50_Trk30_eta2p1_v"
+        "HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET90_v",
+        "HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET100_v",
+        "HLT_MediumChargedIsoPFTau50_Trk30_eta2p1_1pr_MET110_v",
+        "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v",
+        "HLT_MediumChargedIsoPFTau100HighPtRelaxedIso_Trk50_eta2p1_1pr_v",
+        "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_1pr_v",
+        "HLT_PFMETNoMu120_PFMHTNoMu120_IDTight_v",
     ),
 # FIXME: Temporarily CaloMET to emulate trigger MET leg 09062016/SL
     METCollection  = cms.InputTag("slimmedMETs"),
