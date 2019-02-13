4458e461488dff50a72988b78ab6d738330701e9
Marina Kolosova <marina.kolosova@cern.ch>
2019-02-13 08:03:40 -0600

    Data Format: Updated all auto-generated files

--- NtupleAnalysis/src/DataFormat/src/JetGenerated.cc
+++ NtupleAnalysis/src/DataFormat/src/JetGenerated.cc
@@ -10,10 +10,8 @@ void JetGeneratedCollection::setupBranches(BranchManager& mgr) {
   ParticleCollection::setupBranches(mgr);
   fMCjet.setupBranches(mgr);
 
-  mgr.book(prefix()+"_IDloose", &fIDloose);
   mgr.book(prefix()+"_IDtight", &fIDtight);
   mgr.book(prefix()+"_IDtightLeptonVeto", &fIDtightLeptonVeto);
-  mgr.book(prefix()+"_PUIDloose", &fPUIDloose);
   mgr.book(prefix()+"_PUIDmedium", &fPUIDmedium);
   mgr.book(prefix()+"_PUIDtight", &fPUIDtight);
   mgr.book(prefix()+"_originatesFromChargedHiggs", &fOriginatesFromChargedHiggs);
@@ -21,16 +19,25 @@ void JetGeneratedCollection::setupBranches(BranchManager& mgr) {
   mgr.book(prefix()+"_originatesFromUnknown", &fOriginatesFromUnknown);
   mgr.book(prefix()+"_originatesFromW", &fOriginatesFromW);
   mgr.book(prefix()+"_originatesFromZ", &fOriginatesFromZ);
-  mgr.book(prefix()+"_AK4PFCHSpileupJetIdEvaluatorfullDiscriminant", &fAK4PFCHSpileupJetIdEvaluatorfullDiscriminant);
-  mgr.book(prefix()+"_QGTaggerAK4PFCHSaxis2", &fQGTaggerAK4PFCHSaxis2);
-  mgr.book(prefix()+"_QGTaggerAK4PFCHSptD", &fQGTaggerAK4PFCHSptD);
-  mgr.book(prefix()+"_QGTaggerAK4PFCHSqgLikelihood", &fQGTaggerAK4PFCHSqgLikelihood);
+  mgr.book(prefix()+"_QGTaggerqgLikelihood", &fQGTaggerqgLikelihood);
+  mgr.book(prefix()+"_axis1", &fAxis1);
+  mgr.book(prefix()+"_axis2", &fAxis2);
+  mgr.book(prefix()+"_caloJetMapemEnergyFraction", &fCaloJetMapemEnergyFraction);
+  mgr.book(prefix()+"_caloJetMappt", &fCaloJetMappt);
+  mgr.book(prefix()+"_charge", &fCharge);
   mgr.book(prefix()+"_pileupJetIdfullDiscriminant", &fPileupJetIdfullDiscriminant);
+  mgr.book(prefix()+"_ptD", &fPtD);
+  mgr.book(prefix()+"_pullPhi", &fPullPhi);
+  mgr.book(prefix()+"_pullRap", &fPullRap);
   mgr.book(prefix()+"_pfCombinedCvsBJetTags", &fPfCombinedCvsBJetTags);
   mgr.book(prefix()+"_pfCombinedCvsLJetTags", &fPfCombinedCvsLJetTags);
   mgr.book(prefix()+"_pfCombinedInclusiveSecondaryVertexV2BJetTags", &fPfCombinedInclusiveSecondaryVertexV2BJetTags);
   mgr.book(prefix()+"_pfCombinedMVAV2BJetTags", &fPfCombinedMVAV2BJetTags);
   mgr.book(prefix()+"_pfCombinedSecondaryVertexV2BJetTags", &fPfCombinedSecondaryVertexV2BJetTags);
+  mgr.book(prefix()+"_pfDeepCSVBJetTags", &fPfDeepCSVBJetTags);
+  mgr.book(prefix()+"_pfDeepCSVCvsBJetTags", &fPfDeepCSVCvsBJetTags);
+  mgr.book(prefix()+"_pfDeepCSVCvsLJetTags", &fPfDeepCSVCvsLJetTags);
+  mgr.book(prefix()+"_pfDeepFlavourBJetTags", &fPfDeepFlavourBJetTags);
   mgr.book(prefix()+"_pfJetBProbabilityBJetTags", &fPfJetBProbabilityBJetTags);
   mgr.book(prefix()+"_pfJetProbabilityBJetTags", &fPfJetProbabilityBJetTags);
   mgr.book(prefix()+"_pfSimpleInclusiveSecondaryVertexHighEffBJetTags", &fPfSimpleInclusiveSecondaryVertexHighEffBJetTags);
@@ -38,11 +45,7 @@ void JetGeneratedCollection::setupBranches(BranchManager& mgr) {
   mgr.book(prefix()+"_pfTrackCountingHighEffBJetTags", &fPfTrackCountingHighEffBJetTags);
   mgr.book(prefix()+"_softPFElectronBJetTags", &fSoftPFElectronBJetTags);
   mgr.book(prefix()+"_softPFMuonBJetTags", &fSoftPFMuonBJetTags);
-  mgr.book(prefix()+"_tightpfCombinedCvsBJetTags", &fTightpfCombinedCvsBJetTags);
-  mgr.book(prefix()+"_tightpfCombinedCvsLJetTags", &fTightpfCombinedCvsLJetTags);
-  mgr.book(prefix()+"_tightpfCombinedInclusiveSecondaryVertexV2BJetTags", &fTightpfCombinedInclusiveSecondaryVertexV2BJetTags);
-  mgr.book(prefix()+"_tightpfCombinedSecondaryVertexV2BJetTags", &fTightpfCombinedSecondaryVertexV2BJetTags);
-  mgr.book(prefix()+"_QGTaggerAK4PFCHSmult", &fQGTaggerAK4PFCHSmult);
   mgr.book(prefix()+"_hadronFlavour", &fHadronFlavour);
+  mgr.book(prefix()+"_mult", &fMult);
   mgr.book(prefix()+"_partonFlavour", &fPartonFlavour);
 }
